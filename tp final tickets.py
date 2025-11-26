dd9231ec6af535e954b78c52900bd83c72ea8428
Facuu <facundoblas2020@gmail.com>
2025-11-26 17:11:01 -0300

    tp final tickets.py

--- tp final tickets.py
+++ tp final tickets.py
@@ -1,9 +1,10 @@
-import random
-import json
-array_dics = []
+import json, sys, os, random
+array_tkts = []
 
+#def main():
 def main():
     while True:
+        os.system('cls')
         print ('''\n \n══════ Sistema de tickets ══════\n''')
         print ('Por favor, seleccione de las siguientes opciones: \n \n 1: Generar un nuevo ticket. \n 2: Leer un ticket. \n 3: Salir del sistema. \n')
         rta = input('Ingrese su número: ').lower().strip()
@@ -28,22 +29,23 @@ def main():
         if rta == '3':
             salir = salir_programa()
             if salir == 's':
-                break
-        
+                sys.exit()
+                
 
 #Opcion 1: Genera el ticket y lo guarda
 def alta_tkt():
+    os.system('cls')
     print('\nPerfecto, por favor ingrese:')
     nombre = input('Nombre: ')
     sector = input('Sector: ')
     asunto = input('Asunto: ')
     problema = input('Problema: ')
+    num_ticket = random.randrange(1000, 9999)
     
+    os.system('cls')
     print('''\n \n╔══════════════════════════════════╗
  Se ha generado el siguiente ticket 
 ╚══════════════════════════════════╝''')
-    
-    num_ticket = random.randint(1000, 9999)
     
     print (f'''Su nombre: {nombre.capitalize()}
 N° de ticket: {num_ticket}
@@ -51,9 +53,9 @@ Sector: {sector}
 Asunto: {asunto}\n
 Mensaje: {problema}''')
     print ('\n* POR FAVOR NO OLVIDE SU N° DE TICKET *')
-    
-    #Guardar el ticket:
-    dic = {
+
+    #Guarda el ticket en el archivo json
+    ticket = {
         'nombre' : nombre,
         'sector' : sector,
         'asunto' : asunto,
@@ -61,17 +63,20 @@ Mensaje: {problema}''')
         'mensaje' : problema, 
         }
     
-    array_dics.append(dic)
+    array_tkts.append(ticket)
+    guardar_tkt()
     
-    with open('tickets.json', 'w', encoding='utf-8') as f:
-        json.dump(array_dics,f, indent=4)
-        
+    #Ejecuta la pregunta de generar otro ticket
     rta_otro_tkt = generar_otro_tkt()
-    
     if rta_otro_tkt == 's':
         alta_tkt()
     else:
         main()
+
+#Función que guarda el ticket
+def guardar_tkt():
+    with open('tickets.json', 'w', encoding='utf-8') as f:
+        json.dump(array_tkts, f, indent=4)
 
 #Pregunta si genera otro ticket o vuelve al menu
 def generar_otro_tkt():
@@ -81,8 +86,34 @@ def generar_otro_tkt():
             return rta_otro_tkt
 
         print('Por favor, seleccione una de las dos opciones.')
+
+#Opcion 2: leer el ticket
+def buscar_tkt():
+    tkt_num = leer_tkt()
+    encontrado = False
     
+    if os.path.isfile('tickets.json'):
+        with open('tickets.json', 'r') as f:
+            array_tkts = json.load(f)
+            
+        for ticket in array_tkts:
+            if ticket["num_ticket"] == tkt_num:
+                print(f'''\n═══ Se encontró el siguiente ticket ═══
+Nombre: {ticket['nombre']}
+Sector: {ticket['sector']}
+Asunto: {ticket['asunto']}
+Mensaje: {ticket['mensaje']}\n''')
+                encontrado = True
+                break
+    
+    if not encontrado:
+        print ('\nNo se encontró ningun ticket que coincida con ese número.\n')
+        
+    leer_otro_tkt = leer_otros_tkt()   
+
+#Funcion que pide el n° de ticket a leer
 def leer_tkt():
+    os.system('cls')
     tkt_a_leer = input ('\nPerfecto, por favor ingrese el número del ticket que quiere leer: ').strip()
     
     #Mensaje de error
@@ -96,26 +127,6 @@ def leer_tkt():
     
     return tkt_num
 
-#Buscar el ticket  
-def buscar_tkt():
-    tkt_num = leer_tkt()
-    encontrado = False
-    
-    for ticket in array_dics:
-        if ticket["num_ticket"] == tkt_num:
-            print('\n═══ Se encontró el siguiente ticket ═══')
-            print(f"Nombre: {ticket['nombre']}")
-            print(f"Sector: {ticket['sector']}")
-            print(f"Asunto: {ticket['asunto']}\n")
-            print(f"Mensaje: {ticket['mensaje']}\n")
-            encontrado = True
-            break
-    
-    if not encontrado:
-        print ('\nNo se encontró ningun ticket que coincida con ese número.')
-        
-    leer_otro_tkt = leer_otros_tkt()   
-
 #Pregunta si leer otro ticket
 def leer_otros_tkt():
     while True:
@@ -128,12 +139,14 @@ def leer_otros_tkt():
         else:
             print('Por favor, seleccione una de las dos opciones.')
 
+
 #Opcion 3: salir del programa
 def salir_programa():
+    os.system('cls')
     while True:
-        salir_prog = input ('\n¿Está seguro que desea salir del programa? (s/n): ').lower().strip()
-        if salir_prog in ('s', 'n'):
-            return salir_prog
+        salir_sist = input ('\n¿Está seguro que desea salir del programa? (s/n): ').lower().strip()
+        if salir_sist in ('s', 'n'):
+            return salir_sist
         else:
             print('Por favor, seleccione una de las dos opciones.')
 
